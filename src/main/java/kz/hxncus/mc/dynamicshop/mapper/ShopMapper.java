e83d99cefb51a4f63bf3589aae4006a9b6b69cfb
userp <mansurahmetov3@gmail.com>
2026-03-17 00:12:07 +0500

    Implement ShopMapper: Shop <-> ShopDTO conversion with nested Item mapping

--- src/main/java/kz/hxncus/mc/dynamicshop/mapper/ShopMapper.java
+++ src/main/java/kz/hxncus/mc/dynamicshop/mapper/ShopMapper.java
@@ -4,15 +4,14 @@ import kz.hxncus.mc.dynamicshop.domain.Item;
 import kz.hxncus.mc.dynamicshop.domain.Shop;
 import kz.hxncus.mc.dynamicshop.model.ItemDTO;
 import kz.hxncus.mc.dynamicshop.model.ShopDTO;
+import lombok.NonNull;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class ShopMapper {
-    public static ShopDTO toDTO(Shop shop) {
-        if (shop == null){
-            return null;
-        }
+    public static ShopDTO toDTO(@NonNull Shop shop) {
+
         List<ItemDTO> items = shop.getItems() == null
                 ? null
                 : shop.getItems()
@@ -26,10 +25,8 @@ public class ShopMapper {
         );
     }
 
-    public static Shop toShop(ShopDTO dto) {
-        if (dto == null) {
-            return null;
-        }
+    public static Shop toShop(@NonNull ShopDTO dto) {
+
 
         List<Item> items = dto.getItems() == null
                 ?null
