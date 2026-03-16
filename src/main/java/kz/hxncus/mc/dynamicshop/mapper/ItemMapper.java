66708edc57e0626b0c21794b2d851104911cd3c0
userp <mansurahmetov3@gmail.com>
2026-03-17 00:16:12 +0500

    Implement ItemMapper: Item <-> ItemDTO conversion

--- src/main/java/kz/hxncus/mc/dynamicshop/mapper/ItemMapper.java
+++ src/main/java/kz/hxncus/mc/dynamicshop/mapper/ItemMapper.java
@@ -2,21 +2,18 @@ package kz.hxncus.mc.dynamicshop.mapper;
 
 import kz.hxncus.mc.dynamicshop.domain.Item;
 import kz.hxncus.mc.dynamicshop.model.ItemDTO;
+import lombok.NonNull;
 import org.bukkit.Material;
 
 public class ItemMapper {
-    public static ItemDTO toDTO(Item item) {
-        if (item == null){
-            return null;
-        }
+    public static ItemDTO toDTO(@NonNull Item item) {
+
         return new ItemDTO(0, item.getType().name(), item.getPrice());
 
     }
 
-    public static Item toItem(ItemDTO dto){
-        if (dto == null){
-            return null;
-        }
+    public static Item toItem(@NonNull ItemDTO dto){
+
         return new Item (Material.valueOf(dto.getName()), dto.getPrice());
     }
 
